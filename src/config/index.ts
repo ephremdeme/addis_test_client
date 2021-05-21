3c1cd0d09a39f46bc507dad39d8d5ad1784933ab
ephremdeme <demelashephrem@gmail.com>
2021-05-22 02:40:14 +0300

    📦 NEW: axios instance configration

---
+++ src/config/index.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_URL = "https://addis-test.herokuapp.com/api";
+const API_LOCAL = "http://localhost:5001/api";
+
+export const axiosInstance = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
