5be7b7493ed085f5f72155a11615f0295634a905
ephremdeme <demelashephrem@gmail.com>
2021-05-22 02:52:28 +0300

    🐛 FIX: remove unused vars

--- src/components/EditForm.tsx
+++ src/components/EditForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = "https://addis-test.herokuapp.com/api";
-const API_LOCAL = "http://localhost:5001/api";
+// const API_LOCAL = "http://localhost:5001/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
